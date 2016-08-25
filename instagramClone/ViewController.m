989c7fe648afdf85f21cf6cd96ec326c1edc5413
rnjailamba <rnjailamba12@gmail.com>
2016-08-25 07:03:01 +0530

    Added 3 view controllers and the page view controller is set up

--- instagramClone/ViewController.m
+++ instagramClone/ViewController.m
@@ -12,7 +12,13 @@
 #import "MessageViewController.h"
 
 
-@interface ViewController ()
+@interface ViewController () <UIPageViewControllerDataSource >
+
+@property (strong, nonatomic) UIPageViewController *pageController;
+@property (strong, nonatomic) MainViewController *mainVC;
+@property (strong, nonatomic) StoryViewController *storyVC;
+@property (strong, nonatomic) MessageViewController *messageVC;
+
 
 @end
 
@@ -21,6 +27,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.frame = [[UIScreen mainScreen] bounds ];
+    self.mainVC = [[MainViewController alloc] initWithNibName:@"MainViewController" bundle:nil];
+    self.storyVC = [[StoryViewController alloc] initWithNibName:@"StoryViewController" bundle:nil];
+    self.messageVC = [[MessageViewController alloc] initWithNibName:@"MessageViewController" bundle:nil];
+    
+    self.pageController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
+    
+    self.pageController.dataSource = self;
+    [[self.pageController view] setFrame:[[self view] bounds]];
+    
+    NSArray *viewControllers = [NSArray arrayWithObject:self.mainVC];
+    
+    [self.pageController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+    
+    [self addChildViewController:self.pageController];
+    [[self view] addSubview:[self.pageController view]];
+    [self.pageController didMoveToParentViewController:self];
     // Do any additional setup after loading the view, typically from a nib.
 }
 - (void)viewWillAppear:(BOOL)animated {
@@ -31,6 +53,33 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
+    if(true){
+        return self.messageVC;
+    }
+    
+    
+    
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
+    
+    if(true){
+        return self.messageVC;
+    }
+    
+}
+
+- (NSInteger)presentationCountForPageViewController:(UIPageViewController *)pageViewController {
+    // The number of items reflected in the page indicator.
+    return 3;
+}
+
+- (NSInteger)presentationIndexForPageViewController:(UIPageViewController *)pageViewController {
+    // The selected item reflected in the page indicator.
+    return 0;
 }
 
 @end
