1ad5bc685be25e53878dcf24a88237f74cd9e74b
rnjai <rnjailamba12@gmail.com>
2016-08-29 10:08:34 +0530

    added cells to the table view

--- instagramClone/MainViewController.m
+++ instagramClone/MainViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "MainViewController.h"
+#import "FeedCell.h"
 
-@interface MainViewController ()
+
+@interface MainViewController ()<UITableViewDataSource,UITableViewDelegate>
 
 - (IBAction)mailClicked:(id)sender;
 - (IBAction)addClicked:(id)sender;
@@ -21,12 +23,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.frame = [[UIScreen mainScreen] bounds];
+    [self registerNib];
+    [self setUpTableView];
     // Do any additional setup after loading the view from its nib.
+}
+
+-(void)setUpTableView{
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+}
+
+-(void)registerNib{
+    [self.tableView registerNib:[UINib nibWithNibName:@"FeedCell" bundle:nil] forCellReuseIdentifier:@"cell"];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma UITableViewDataSource
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *MyIdentifier = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MyIdentifier];
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 5;    //count number of row from counting array hear cataGorry is An Array
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;    //count of section
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    return self.view.frame.size.width + 120.5;
+    
 }
 
 /*
